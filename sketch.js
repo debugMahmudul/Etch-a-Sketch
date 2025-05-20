41fa8bcf427da07761f63c44797edb21e333798c
debugMahmudul <mahmudul.hamim10@gmail.com>
2025-05-19 22:34:38 -0300

    Add border to the grid

--- sketch.js
+++ sketch.js
@@ -2,7 +2,7 @@
 const container = document.querySelector(".container");
 
 for (let i = 0; i < 256; i++) {
-    
+
     const squareDiv = document.createElement("div");
     squareDiv.classList.add("square-div");
     container.appendChild(squareDiv);
