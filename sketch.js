e59d61352fe086d1c80ecf12b012717dc6d99d80
debugMahmudul <mahmudul.hamim10@gmail.com>
2025-05-19 21:34:45 -0300

    Create html structure for the page and add container div to store all the square div - create a for loop in js to create 256 divs and store them in container -append the square divs to the container - add display:flex in the parent container to make it a flex element - add width and aspect ratio in each square div to make them an even square

---
+++ sketch.js
@@ -0,0 +1,10 @@
+
+const container = document.querySelector(".container");
+
+for (let i = 0; i < 256; i++) {
+    
+    const squareDiv = document.createElement("div");
+    squareDiv.classList.add("square-div");
+    container.appendChild(squareDiv);
+}
+
