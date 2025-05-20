f282ea5921c7805c833d10da9a87d474161e9a71
debugMahmudul <mahmudul.hamim10@gmail.com>
2025-05-20 16:48:21 -0300

    Add a hover effect on the grid

--- sketch.js
+++ sketch.js
@@ -4,7 +4,20 @@ const container = document.querySelector(".container");
 for (let i = 0; i < 256; i++) {
 
     const squareDiv = document.createElement("div");
+
     squareDiv.classList.add("square-div");
+
     container.appendChild(squareDiv);
+
+    squareDiv.addEventListener("mouseover", event => {
+        event.target.style.backgroundColor = "tomato";
+    });
 }
+
+
+
+
+
+
+
 
