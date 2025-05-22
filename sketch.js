752a7476d70368cfd1674a4ce0373a502480eec5
debugMahmudul <mahmudul.hamim10@gmail.com>
2025-05-21 22:00:29 -0300

    Create a button with event listener it will prompt user for the customizable grid size add condition for input validation add a for loop logic to create new customizable grid add a event listener to create hover effect on the new grid

--- sketch.js
+++ sketch.js
@@ -1,18 +1,68 @@
 
 const container = document.querySelector(".container");
 
+//Loop to create the 16x16 grid. 
 for (let i = 0; i < 256; i++) {
 
     const squareDiv = document.createElement("div");
 
     squareDiv.classList.add("square-div");
-
     container.appendChild(squareDiv);
 
-    squareDiv.addEventListener("mouseover", event => {
-        event.target.style.backgroundColor = "tomato";
+    squareDiv.addEventListener("mouseover", () => {
+        squareDiv.style.backgroundColor = "tomato";
     });
-}
+};
+
+//create a button
+const button = document.querySelector("button");
+
+//Create a button to receive gride size from the user
+button.addEventListener("click", () => {
+    let userInputString = prompt("Enter the number of squares per side (up to 100):");
+
+    //Check for input validation
+    if (userInputString === null) {
+        alert("User cancelled the input");
+        return;
+    };
+    const userInputNumber = parseInt(userInputString);
+
+    if (isNaN(userInputNumber)) {
+        alert("Invalid input: Not a number. Please enter a number between 1 and 100.");
+        return;
+    }
+
+    if (userInputNumber < 1 || userInputNumber > 100) {
+        alert("Invalid input: Number must be between 1 and 100.");
+        return;
+    }
+
+    //This is to clear the container before we create the new grid
+    container.innerHTML = "";
+
+    //Calculate the percentage width of the grid.
+    const percentageWidth = (100 / userInputNumber);
+
+    for (let i = 0; i < userInputNumber * userInputNumber; i++) {
+
+        const squareDiv = document.createElement("div");
+        squareDiv.classList.add("square-div");
+        container.appendChild(squareDiv);
+        squareDiv.style.width = percentageWidth + "%";
+
+
+        squareDiv.addEventListener("mouseover", () => {
+            squareDiv.style.backgroundColor = "tomato";
+        });
+    }
+});
+
+
+
+
+
+
 
 
 
